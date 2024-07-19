f916c09c01128a0cd40bbb1a30d5695119e56e46
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-07-19 00:15:57 -0300

    commit inicial

---
+++ GerenciadorTextos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorTextos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
